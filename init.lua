910f3387a2e9193d86f2e5d144427f721cc49746
rockandrollprophet <amclark2023@gmail.com>
2026-01-31 03:57:48 -0500

    feat(plugins): add lazy.nvim and plugin configuration

--- init.lua
+++ init.lua
@@ -41,8 +41,38 @@ end
 -- Call the functions defined above.
 load_sources({
   "base.1-options",
-  "base.2-lazy",
+  -- Modern plugin manager: lazy.nvim
+  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+  if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+      "git", "clone", "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git", lazypath
+    })
+  end
+  vim.opt.rtp:prepend(lazypath)
+  require("lazy").setup(require("plugins.plugins"))
+  -- End lazy.nvim setup
   "base.3-autocmds", -- critical stuff, don't change the execution order.
 })
 load_colorscheme(vim.g.default_colorscheme)
 load_sources_async({ "base.4-mappings" })
+
+-- Modern Neovim settings (add more as needed)
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.smartindent = true
+vim.opt.cursorline = true
+vim.opt.clipboard = "unnamedplus"
+
+-- Theme (pick one)
+vim.cmd.colorscheme "catppuccin"
+-- vim.cmd.colorscheme "tokyonight"
+-- vim.cmd.colorscheme "onedark"
+-- vim.cmd.colorscheme "rose-pine"
+
+-- Add more config as needed for LSP, completion, treesitter, etc.
+-- See plugin docs for details.
